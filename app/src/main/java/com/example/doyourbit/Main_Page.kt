e2aae170ac6a2fea57b95fbacb2212b9676dc8bc
IshaanVerma1130 <IshaanVerma1130@users.noreply.github.com>
2020-09-07 16:06:43 +0530

    first

--- app/src/main/java/com/example/doyourbit/Main_Page.kt
+++ app/src/main/java/com/example/doyourbit/Main_Page.kt
@@ -3,7 +3,7 @@ package com.example.doyourbit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Main_Page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main__page)
