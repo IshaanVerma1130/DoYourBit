1cf2fc3eb666cb1eb2dd5eeef82bb228305f9b60
IshaanVerma1130 <IshaanVerma1130@users.noreply.github.com>
2020-09-09 17:49:17 +0530

    ishaan

--- app/src/main/java/com/example/doyourbit/MainActivity.kt
+++ app/src/main/java/com/example/doyourbit/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main__page)
+        setContentView(R.layout.activity_main)
     }
 }
-//ishaan verma
